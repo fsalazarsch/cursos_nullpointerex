1327c278ca7c2398bea20118620a246498e3427d
fsalazarsch <fsalazar.sch@gmail.com>
2016-04-06 16:25:18 -0300

    Create variables.c

---
+++ C/variables.c
@@ -0,0 +1,17 @@
+
+<stdio.h>
+
+void main(){
+
+int i = 5;l
+float f = 2,4;
+char c = 'b';
+char pal[20] = 'palabra simple';
+
+printf("Escribir variables\n");
+printf("Valor entero: %i\n", i);
+printf("Valor flotante: %f\n", f);
+printf("Valor caracter: %c\n", c);
+printf("Valor caracter: %s\n", pal);
+
+}
