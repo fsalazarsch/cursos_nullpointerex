73dbca7a56abc1f516bf9ecb170e8c2094003d9e
Felipe Salazar Schlotterbeck <fsalazar.sch@gmail.com>
2018-11-23 21:21:45 -0300

    Create Hola_Mundo.cs

---
+++ C#/Ejercicios Resueltos/Curso/Hola_Mundo.cs
@@ -0,0 +1,25 @@
+/*
+ * Created by SharpDevelop.
+ * User: Felipe
+ * Date: 23-11-2018
+ * Time: 21:07
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace codigos_de_prueba
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hola Mundo desde C#");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Presione cualquier tecla para continuar ... ");
+			Console.ReadKey(true);
+		}
+	}
+}
