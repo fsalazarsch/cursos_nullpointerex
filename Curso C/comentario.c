35fb48cc431410cccfc873e108a6b44ea918e05c
Felipe Salazar Schlotterbeck <fsalazar.sch@gmail.com>
2025-05-26 01:31:33 -0400

    Create comentario.c

---
+++ Curso C/comentario.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    //Este es un comentario de una linea
+
+    /*
+    Este es 
+    un comentario
+    de varias lineas
+    */
+    printf("Hola mundo desde C\n");
+    
+    printf("Presione una tecla para continuar ...");
+    getchar();
+    return 0;
+
+    } 
+
